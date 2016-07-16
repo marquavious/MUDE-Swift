c876b82b26d4265f28dff3c93a6b3fec4412918b
sbrick26 <swayambarik@gmail.com>
2016-07-16 00:24:02 -0700

    Added pod files / Functionality 🐳💦🍑🤑

---
+++ MUDE-Swift-master 2/Pods/Target Support Files/Pods-Hue Quick Start iOS Swift/Pods-Hue Quick Start iOS Swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hue_Quick_Start_iOS_Swift : NSObject
+@end
+@implementation PodsDummy_Pods_Hue_Quick_Start_iOS_Swift
+@end
---
+++ MUDE-Swift-master 2/Pods/Target Support Files/Pods-Hue Quick Start iOS Swift/Pods-Hue Quick Start iOS Swift-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Hue_Quick_Start_iOS_SwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Hue_Quick_Start_iOS_SwiftVersionString[];
+
