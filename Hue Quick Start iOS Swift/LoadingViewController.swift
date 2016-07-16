bbea04d33ac43998e25399462101541583edbfa2
Marquavious Draggon <marq.draggon@gmail.com>
2016-07-15 22:19:41 -0700

    😁😂😅LETS GOOO!!!!🔥🔥

---
+++ Hue Quick Start iOS Swift/LoadingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoadingViewController.swift
+//  Hue Quick Start iOS Swift
+//
+//  Created by Kevin Dew on 29/01/2015.
+//  Copyright (c) 2015 KevinDew. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+  
+  @IBOutlet var loadingLabel: UILabel?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
